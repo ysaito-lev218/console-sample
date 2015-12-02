1c5b6c0324566b39372106aed66e1d0fe0559325
Yuichi Saito <yuichi.saito@leverages.jp>
2015-12-02 10:18:35 +0900

    add return code class

---
+++ app/Console/ReturnCode.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Console;
+
+class ReturnCode
+{
+    /**
+     * Success code.
+     */
+    const SUCCESS = 0;
+
+    /**
+     * Failure code.
+     */
+    const FAIL    = 9;
+}
