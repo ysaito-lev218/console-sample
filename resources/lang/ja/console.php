1360a57013db45e741a4d17a3c246d44ddce816b
Yuichi Saito <yuichi.saito@leverages.jp>
2015-12-02 18:09:12 +0900

    メッセージファイル追加

---
+++ resources/lang/ja/console.php
@@ -0,0 +1,15 @@
+<?php  // resources/lang/ja/console.php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Console Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'start'   => ':name が起動しました。',
+    'success' => ':name が正常終了しました。',
+    'fail'    => ':name が異常終了しました。',
+    'already' => 'The command is already running in another process.',
+];
